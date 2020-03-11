50f92e7a29fb25915ee701cefecc14adfcec4125
JabinWashburn <jabinwashburn@gmail.com>
2020-03-11 15:22:23 -0700

    finished

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@
 
 
 //here is where i made the home nav turn red when hovering, it resets every hover.
+
 function hoverEventHandler(e){
   e.target.style.color = 'red';
 
@@ -24,6 +25,7 @@ function hoverEventHandler(e){
 document.querySelector('a').addEventListener('mouseover', hoverEventHandler);
 
 // mouseover end 
+//you will be alrted to check the console and you will see the button you pressed 
 
 
 window.addEventListener('keydown', (e) =>{
@@ -31,7 +33,7 @@ window.addEventListener('keydown', (e) =>{
   console.log(e.key);
 });
 //keydown end :) this one is funny go do it. 
-
+//using the scroll wheel on the bus image changes its size. 
 
 function zoom(e){
   e.preventDefault();
@@ -47,7 +49,7 @@ let scale = 1;
 const el = document.querySelector('img');
 el.onwheel = zoom;
 // zoom end here 
-
+//you can drag and drop the nav links around each other, the area to drop them turns purple. 
 
 var dragged;
 
@@ -91,3 +93,67 @@ document.addEventListener('drop', function(e){
     e.target.appendChild(dragged);
   }
 }, false);
+// this is the end of drag/drop 
+//when you load the page and open the console you will see a cool load message to let you know everything was loaded properly. 
+
+window.addEventListener('load', (e) => {
+  console.log('Sup dude this page is fully loaded kinda like a suprem taco from taco bell.')
+})
+
+// this is the end of load 
+//if you click in the input box it will turn pink 
+
+const focusStuff = document.querySelector('input');
+
+focusStuff.addEventListener('focus', (e) =>{
+  e.target.style.background = 'pink';
+}); 
+
+//this is the end of Focus 
+//if you resize the webpage it will alert you the new size of the web page
+window.addEventListener('resize', reportWindowSize);
+
+function reportWindowSize() {
+   alert(`Height: ${window.innerHeight} ,Width: ${window.innerWidth}`);
+}
+
+//this is the end of resize 
+//if you scoll on the page it will log in the console the location of your scroll
+let last_known_scroll_position = 0;
+let ticketing = false; 
+
+function doSomething(scroll_pos){
+  console.log('here is your scroll position',scroll_pos); 
+}
+
+window.addEventListener('scroll', function(e){
+  last_known_scroll_position = window.scrollY;
+
+  if(!ticketing) {
+    window.requestAnimationFrame(function(){
+      doSomething(last_known_scroll_position);
+    });
+
+    ticketing = true;
+  }
+});
+
+//here is the end scroll 
+//start of select., when you highlight it will log in the console what you highlighted in the input 
+function logSelection(e){
+  let selection = e.target.value.substring(e.target.selectionStart, e.target.selectionEnd);
+  console.log('You selected: ' + selection);
+}
+
+const input = document.querySelector('input');
+input.addEventListener('select', logSelection);
+
+
+ //here is the end of select 
+ //start of double click, turns the background green 
+
+ const doubleThing = document.querySelector('div');
+
+ doubleThing.addEventListener('dblclick', function(e){
+   e.target.style.background = 'green';
+ });
